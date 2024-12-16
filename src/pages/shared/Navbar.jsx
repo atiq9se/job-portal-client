b553eb80809140b033094421debf553ec82dd70d
Md.Atiqur Rahman <atiq9se@gmail.com>
2024-12-16 22:46:50 +0600

    logo icon adding

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext/AuthContext';
+import jobIcon from "../../assets/job-logo.png"
 
 const Navbar = () => {
     const {user, signOutUser} = useContext(AuthContext)
@@ -16,7 +17,7 @@ const Navbar = () => {
     }
 
     const links = <>
-        <li><a>Item 1</a></li>
+        <li><NavLink to="/">Home</NavLink></li>
         <li><a>Item 2</a></li>
         <li><a>Item 3</a></li>
     </>
@@ -46,7 +47,10 @@ const Navbar = () => {
 
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">
+                    <img className='w-12' src={jobIcon} alt="" />
+                    <h3 className='text-3xl'>Job Portal</h3>
+                </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
