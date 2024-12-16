4d46346ef2342ff0f03551919cc76593242d391b
Md.Atiqur Rahman <atiq9se@gmail.com>
2024-12-16 23:18:34 +0600

    banner part adding

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,27 @@
+
+import { motion } from "motion/react"
+
+const Banner = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+               <div className="flex-1">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    className="max-w-sm rounded-lg shadow-2xl" />
+               </div>
+                <div className="flex-1">
+                    <motion.h1 animate={{x: 100}} className="text-5xl font-bold">Latest job for you!</motion.h1>
+                    <h1 className="text-5xl font-bold">Latest job for you!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
